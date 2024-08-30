8b8cc755abd8b3f17a60399b1708a2a9d918ce06
begizew <begizewzerihun@gmail.com>
2024-08-30 19:32:46 -0400

    analysis process task 2

--- task1/scripts/preprocess.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def load_and_preprocess_data(file_path):
-    df = pd.read_csv(file_path)
-    df['date'] = pd.to_datetime(df['date'], errors='coerce')
-    df['date'] = df['date'].dt.tz_localize(None)  # Remove timezone information
-    df.to_csv('preprocessed_data.csv', index=False)  # Save preprocessed data
-    return df
